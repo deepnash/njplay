758cb8613ce8a65bd638eebf6326f32cc373795c
Deep Nash <deepnash.dev@gmail.com>
2020-10-12 16:49:24 -0700

    stuff

---
+++ db/test/parse-rdf-test.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const fs = require('fs');
+
+const expect = require('chai').expect;
+
+const parseRDF = require('../lib/parse-rdf.js');
+
+const rdf_filename = `${__dirname}/pg132.rdf`;
+console.log("RDF Filename: ", rdf_filename);
+
+const rdf = fs.readFileSync(rdf_filename);
+
+describe('parseRDF', () => {
+  it('should be a function', () => {
+    expect(parseRDF).to.be.a('function');
+  });
+  it('should parse RDF content', () => {
+    const book = parseRDF(rdf);
+    expect(book).to.be.a('object');
+    expect(book).to.have.a.property('id', 132);
+    expect(book).to.have.a.property('title', 'The Art of War');
+    expect(book).to.have.a.property('authors').that.is.an('array').with.lengthOf(2)
+      .and.contains('Sunzi, active 6th century B.C.')
+      .and.contains('Giles, Lionel');
+
+    expect(book).to.have.a.property('subjects')
+      .that.is.an('array').with.lengthOf(2)
+      .and.contains('Military art and science -- Early works to 1800')
+      .and.contains('War -- Early works to 1800');
+  });
+});
